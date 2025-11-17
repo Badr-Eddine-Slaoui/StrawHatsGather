05939f9f2f1b94133d3af66d3267e2a9e1e32ae3
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-11-17 14:25:56 +0100

    Add Global Variables For Modal Logic

--- globalVariables.js
+++ globalVariables.js
@@ -6,3 +6,13 @@ export let available_roles = [
     "visiter",
     "receptionist"
 ];
+
+
+export let worker_list = document.getElementById("worker-list");
+export let no_worker_in_list = document.getElementById("no-worker-in-list");
+export let add_worker_btn = document.getElementById("add-worker-btn");
+export let add_worker_modal = document.getElementById("add-worker-modal");
+export let close_add_worker_modal = add_worker_modal.querySelector("#close-modal");
+export let add_worker_form = document.getElementById("add-worker-form");
+export let add_experience_btn = document.getElementById("add-experience-btn");
+export let experiences = document.getElementById("experiences");
